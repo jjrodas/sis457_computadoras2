fa9b9c1c7f14ab8eb831621a3f7127ddf3937322
Juan Jose Rodas Paco <geometrycalero030400@gmail.com>
2023-11-12 15:27:20 -0400

    Creación de la clase CategoriaCln

---
+++ ClnComputadoras2/CategoriaCln.cs
@@ -0,0 +1,66 @@
+﻿using CadComputadoras2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnComputadoras2
+{
+    public class CategoriaCln
+    {
+        public static int insertar(Categoria categoria)
+        {
+            using (var context = new LabComputadoras2Entities())
+            {
+                context.Categoria.Add(categoria);
+                context.SaveChanges();
+                return categoria.id;
+            }
+        }
+        public static int actualizar(Categoria categoria)
+        {
+            using (var context = new LabComputadoras2Entities())
+            {
+                var existente = context.Categoria.Find(categoria.id);
+                existente.nombre = categoria.nombre;
+                existente.descripcion = categoria.descripcion;
+                existente.usuarioRegistro = categoria.usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+        public static int eliminar(int id, string usuarioRegistro)
+        {
+            using (var context = new LabComputadoras2Entities())
+            {
+                var existente = context.Categoria.Find(id);
+                existente.estado = -1;
+                existente.usuarioRegistro = usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+        public static Categoria get(int id)
+        {
+            using (var context = new LabComputadoras2Entities())
+            {
+                return context.Categoria.Find(id);
+            }
+        }
+
+        public static List<Categoria> listar()
+        {
+            using (var context = new LabComputadoras2Entities())
+            {
+                return context.Categoria.Where(x => x.estado != -1).ToList();
+            }
+        }
+
+        public static List<paCategoriaListar_Result> listarPa(string parametro)
+        {
+            using (var context = new LabComputadoras2Entities())
+            {
+                return context.paCategoriaListar(parametro).ToList();
+            }
+        }
+    }
+}
