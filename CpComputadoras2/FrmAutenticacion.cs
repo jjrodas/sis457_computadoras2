15ad013b629d95ce77b53d94640607bac8ae9281
Juan José Rodas Paco <geometrycalero030400@gmail.com>
2023-11-18 06:26:13 -0400

    Creación de FrmAutenticacion.

---
+++ CpComputadoras2/FrmAutenticacion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CpComputadoras2
+{
+    public partial class FrmAutenticacion : Form
+    {
+        public FrmAutenticacion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            new FrmPrincipal().ShowDialog();
+        }
+    }
+}
--- CpComputadoras2/Program.cs
+++ CpComputadoras2/Program.cs
@@ -16,7 +16,7 @@ namespace CpComputadoras2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmPrincipal());
+            Application.Run(new FrmAutenticacion());
         }
     }
 }
