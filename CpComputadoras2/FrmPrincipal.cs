a14e163bcad5eb69613409ac02ade882910dc20e
Juan José Rodas Paco <geometrycalero030400@gmail.com>
2023-11-18 00:10:04 -0400

    Creación y diseño del Formulario principal.

---
+++ CpComputadoras2/FrmPrincipal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CpComputadoras2
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCerrarApp_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnProdcutos_Click(object sender, EventArgs e)
+        {
+            new FrmProducto().ShowDialog();
+        }
+
+        private void btnClientes_Click(object sender, EventArgs e)
+        {
+            new FrmCliente().ShowDialog();
+        }
+
+        private void btnEmpleados_Click(object sender, EventArgs e)
+        {
+            new FrmEmpleado().ShowDialog();
+        }
+    }
+}
--- CpComputadoras2/Program.cs
+++ CpComputadoras2/Program.cs
@@ -16,7 +16,7 @@ namespace CpComputadoras2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmCliente());
+            Application.Run(new FrmPrincipal());
         }
     }
 }
